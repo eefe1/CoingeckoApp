fcf1d77c80accdf96336f278e14f7b721616068f
ezgi <esgiefe@gmail.com>
2023-04-24 16:10:16 +1000

    fix:formatted

--- server/index.js
+++ server/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-//allows all the routes to be accessed anywhere on the web.
-  app.use(cors())
+app.use(cors());
 
-  const { getCoins, getCoinById } = require('./api');
+const { getCoins, getCoinById } = require("./api");
 
-  app.get('/coins', async (req, res) => {
-    const skip = parseInt(req.query.skip) || 0;
-    const limit = parseInt(req.query.limit) || 100;
-    const coins = await getCoins(skip, limit);
-    res.send(coins);
-  });
+app.get("/coins", async (req, res) => {
+  const skip = parseInt(req.query.skip) || 0;
+  const limit = parseInt(req.query.limit) || 100;
+  const coins = await getCoins(skip, limit);
+  res.send(coins);
+});
 
-  app.get('/coins/:id', async (req, res) => {
-    const { id } = req.params;
-    const coin = await getCoinById(id);
-    res.send(coin);
-  });
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+app.get("/coins/:id", async (req, res) => {
+  const { id } = req.params;
+  const coin = await getCoinById(id);
+  res.send(coin);
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
